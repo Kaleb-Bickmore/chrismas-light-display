150cd54ebf576906a20868629be09f3a00e4fd79
Kaleb-Bickmore <kbic17@gmail.com>
2021-11-13 08:08:39 -0700

    updating wheel

--- LightModes/SplitWaveStrategy.py
+++ LightModes/SplitWaveStrategy.py
@@ -5,25 +5,32 @@ from Pixels import Pixels
 class SplitWaveStrategy:
     def __init__(self):
         self._pixels = Pixels()
-    def wheel(self, pos):
+    def wheel(self, i):
     # Input a value 0 to 255 to get a color value.
     # The colours are a transition r - g - b - back to r.
-        pos = pos%255
-        if pos < 85:
-            r = int(pos * 3)
-            g = int(255 - pos * 3)
-            b = 0
-        elif pos < 170:
-            pos -= 85
-            r = int(255 - pos * 3)
-            g = 0
-            b = int(pos * 3)
-        else:
-            pos -= 170
-            r = 0
-            g = int(pos * 3)
-            b = int(255 - pos * 3)
-        return (r, g, b)
+        if(i<50):
+            return (255,0,0)
+
+        if(i>=50 and i <100):
+            return (0,255,0)
+
+        if(i>=100 and i <150):
+            return (255,255,255)
+
+        if(i>=150 and i <200):
+            return (255,0,0)
+
+        if(i>=200 and i <250):
+            return (0,255,0)
+
+        if(i>=250 and i <300):
+            return (255,255,255)
+
+        if(i>=300 and i <350):
+            return (255,0,0)
+
+        if(i>=350):
+            return (0,255,0)
     def run(self, cycle_time, bps):
         t_end = time.time() + cycle_time
         while(t_end>=time.time()):
@@ -33,7 +40,7 @@ class SplitWaveStrategy:
 
             for i in range(0, split, step):
                 for j in range(ratio):
-                    self._pixels._pixels[i+split*j:i+step+split*j] = [self.wheel(aa) for aa in self._pixels._pixels[i+split*j:i+step+split*j]]
+                    self._pixels._pixels[i+split*j:i+step+split*j] = [ self.wheel(i) for aa in self._pixels._pixels[i+split*j:i+step+split*j]]
                 self._pixels._pixels.show()
 
             for i in range(split,step,-1*step):
